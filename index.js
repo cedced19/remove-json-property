8698572e70f9db7c45df1d8a0b2f445a0ef1e6c5
Cédric JUNG <cedced19@gmail.com>
2022-02-11 15:56:15 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+
+let input = require('./input.json');
+
+let toExclude = ['sectorLink', 'themeLink'];
+
+let output = [];
+input.forEach(function(obj) {
+    let out = obj;
+    for (let key in toExclude) {
+        delete out[key];
+    }
+    output.push(out);
+})
+
+fs.writeFileSync('./output.json', JSON.stringify(output), 'utf8');
